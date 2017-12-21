efc337742f0362b321225909ce9e743dee04b068
Smik Lakhani <smiklakhani@gmail.com>
2017-12-21 13:45:32 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+// Setup modules
+const restify = require('restify');
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+
+// Prepare global constants
+const server = restify.createServer();
+let greetingCount = 0;
+
+// Configure middleware
+server.use(morgan('common', {
+  stream: fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+}));
+
+// Define response for root
+server.get('rest', (req, res, next) => {
+  res.send(200);
+  next();
+});
+
+// Define routes
+server.get('rest/hello/:name', (req, res, next) => {
+  res.json({ message: `Hello ${req.params.name}. '/hello/:name' has been invoked ${++greetingCount} times since I was last started up.`});
+  next();
+});
+
+// Start server
+server.listen(8080, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
